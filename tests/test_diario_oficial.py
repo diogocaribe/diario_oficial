c898323e2e806c54eccaef8c2ade55008f122dbb
Diogo Caribé <dogosousa@gmail.com>
2024-05-20 14:17:37 -0300

    Adicionando docstring ao test

--- tests/test_diario_oficial.py
+++ tests/test_diario_oficial.py
@@ -1,3 +1,4 @@
+"""Testando a respagem de dados."""
 from diario_oficial.diario_oficial import DiarioOficialBa
 
 
@@ -5,9 +6,12 @@ diario_oficial = DiarioOficialBa()
 
 
 def test_navegador():
+    """_summary_
+    """
     diario_oficial.abrir_site()
 
 def test_clicar_versao_html():
+    """_summary_
+    """
     diario_oficial.clicar_versao_html()
     print('oi')
-
