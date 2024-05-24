221f332d5b7f060fd7999665b53b7287cbd46b52
Diogo Caribé <dogosousa@gmail.com>
2024-05-24 15:07:28 -0300

    Removendo a classe e criando uma rotina de execulção linter do código

--- diario_oficial/diario_oficial.py
+++ diario_oficial/diario_oficial.py
@@ -1,58 +1,68 @@
 """Codigo para raspar dados do DOE."""
+from functools import partial
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 import time
 
-class DiarioOficialBa:
+site_link = "https://dool.egba.ba.gov.br/"
+data = "17-05-2024"
+
+driver = webdriver.Chrome()
+
+# Sintaxe do wait
+wdw = WebDriverWait(
+    driver,  # webdriver
+    timeout=10,  # tempo de espera pelo erro
+    # poll_frequency=0.5,  # tempo entre uma tentativa e outra
+    # ignored_exceptions=None, # Lista de coisas que vamos ignorar
+)
+# Abrindo a pagina principal do diário oficial
+driver.get(site_link)
+
+# Clicar no botão da versão html do diário oficial
+driver.find_element(By.ID, "downloadHTML").click()
+
+# Clicar no botão para continuar sem cadastro
+def esperar_elemento(by, elemento, driver):
     """_summary_
+
+    Args:
+        by (_type_): _description_
+        elemento (_type_): _description_
+        driver (_type_): _description_
+
+    Returns:
+        _type_: _description_
     """
-    def __init__(self) -> None:
-        self.site_link = "https://dool.egba.ba.gov.br/"
-        self.data = '17-05-2024'
+    print(f'Tentando encontrar "{elemento}" by {by}')
+    if driver.find_elements(by, elemento):
+        return True
+    return False
 
-    def site(self):
-        """_summary_
-        """
-        time.sleep(1)
-        driver = webdriver.Chrome()
-        time.sleep(1)
-        driver.get(self.site_link)
-        time.sleep(1)
-        driver.find_element(By.ID, "downloadHTML").click()
-        time.sleep(1)
-        driver.find_element(By.CLASS_NAME, "modal-footer").find_element(
-            By.TAG_NAME, "button"
-        ).click()
-        time.sleep(1)
-        driver.find_element(By.CLASS_NAME, "date-input").clear()
-        time.sleep(5)
-        driver.switch_to.alert.accept()
-        time.sleep(2)
-        driver.find_element(By.CLASS_NAME, "date-input").send_keys(self.data)
-        time.sleep(2)
-        driver.find_element(By.CLASS_NAME, "date-input").click()
-        time.sleep(2)
-        driver.find_element(By.CLASS_NAME, "fa-search").click()
-        time.sleep(5)
-        driver.find_element(By.CLASS_NAME, "modal-footer").find_element(
-            By.TAG_NAME, "button"
-        ).click()
-        time.sleep(60)
+# Janela para seleção de continuar na versão html
+wdw.until(partial(esperar_elemento, By.CLASS_NAME, 'modal-footer'))
+driver.find_element(By.CLASS_NAME, "modal-footer").find_element(
+    By.TAG_NAME, "button"
+).click()
 
-    def clicar_versao_html(self):
-        """_summary_
-        """
-        time.sleep(2)
-        # self.driver.find_element(By.ID, "downloadHTML").click()
-        time.sleep(60)
+# Selecionando a data
+driver.find_element(By.CLASS_NAME, "date-input").clear()
 
-    def clicar_splashscreen(self):
-        """_summary_
-        """
+time.sleep(2)
+driver.switch_to.alert.accept()
 
+driver.find_element(By.CLASS_NAME, "date-input").send_keys(data)
 
-    def definir_data(self):
-        """_summary_
-        """
+driver.find_element(By.CLASS_NAME, "date-input").click()
 
-DiarioOficialBa().site()
+driver.find_element(By.CLASS_NAME, "fa-search").click()
+
+# Janela para seleção de continuar na versão html
+time.sleep(2)
+wdw.until(partial(esperar_elemento, By.CLASS_NAME, 'modal-footer'))
+driver.find_element(By.CLASS_NAME, "modal-footer").find_element(
+    By.TAG_NAME, "button"
+).click()
+
+time.sleep(60)
---
+++ scripts/diario_oficial.py
@@ -0,0 +1,42 @@
+"""Codigo para raspar dados do DOE."""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+class DiarioOficialBa:
+    """_summary_
+    """
+    def __init__(self) -> None:
+        self.site_link = "https://dool.egba.ba.gov.br/"
+        self.data = '17-05-2024'
+
+    def site(self):
+        """_summary_
+        """
+        time.sleep(1)
+        driver = webdriver.Chrome()
+        time.sleep(1)
+        driver.get(self.site_link)
+        time.sleep(1)
+        driver.find_element(By.ID, "downloadHTML").click()
+        time.sleep(1)
+        driver.find_element(By.CLASS_NAME, "modal-footer").find_element(
+            By.TAG_NAME, "button"
+        ).click()
+        time.sleep(1)
+        driver.find_element(By.CLASS_NAME, "date-input").clear()
+        time.sleep(5)
+        driver.switch_to.alert.accept()
+        time.sleep(2)
+        driver.find_element(By.CLASS_NAME, "date-input").send_keys(self.data)
+        time.sleep(2)
+        driver.find_element(By.CLASS_NAME, "date-input").click()
+        time.sleep(2)
+        driver.find_element(By.CLASS_NAME, "fa-search").click()
+        time.sleep(5)
+        driver.find_element(By.CLASS_NAME, "modal-footer").find_element(
+            By.TAG_NAME, "button"
+        ).click()
+        time.sleep(60)
+
+DiarioOficialBa().site()
