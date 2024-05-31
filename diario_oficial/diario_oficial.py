b697c282cfff77d908ad082910819c971fe2abf3
Diogo Caribé <dogosousa@gmail.com>
2024-05-31 19:58:27 -0300

    Realizando seleção das secretarias (nivel 2 de pasta do sumário) para ser aberto para coleta de dados.

--- diario_oficial/diario_oficial.py
+++ diario_oficial/diario_oficial.py
@@ -11,7 +11,26 @@ import time
 
 site_link = "https://dool.egba.ba.gov.br/"
 data = "17-05-2024"
-selecao_pasta_nivel_1 = ["EXECUTIVO", "LICITAÇÕES", 'MUNICÍPIOS']
+selecao_pasta_nivel_1 = ["EXECUTIVO", "LICITAÇÕES"]
+# TODO implementar a seleção de pastas no nivel 2
+# LIsta de pastas que serão abertas para coleta de dados
+selecao_pasta_nivel_2_executivo = ["SECRETARIA DO MEIO AMBIENTE", "SECRETARIA DE INFRAESTRUTURA"]
+selecao_pasta_nivel_2_licitacao = ["AVISOS DE LICITAÇÃO"]
+selecao_pasta_nivel_2_municipio = []
+selecao_pasta_nivel_2_diverso = []
+selecao_pasta_nivel_2_especial = []
+
+
+def selecao_pasta_nivel_2(
+    lista1=selecao_pasta_nivel_2_executivo,
+    lista2=selecao_pasta_nivel_2_licitacao,
+    lista3=selecao_pasta_nivel_2_municipio,
+    lista4=selecao_pasta_nivel_2_diverso,
+    lista5=selecao_pasta_nivel_2_especial,
+):
+    selecao_pasta_nivel_2 = set(lista1 + lista2 + lista3 + lista4 + lista5)
+    return selecao_pasta_nivel_2
+
 
 navegador = webdriver.Chrome()
 
@@ -41,7 +60,7 @@ def esperar_elemento(by, elemento, navegador):
     Returns:
         _type_: _description_
     """
-    print(f'Tentando encontrar "{elemento}" by {by}')
+    # print(f'Tentando encontrar "{elemento}" by {by}')
     if navegador.find_elements(by, elemento):
         return True
     return False
@@ -73,6 +92,15 @@ navegador.find_element(By.CLASS_NAME, "modal-footer").find_element(
 ).click()
 
 
+# Listar pastas no nivel 1 do sumário
+lista_pasta_nivel_1 = []
+for i in navegador.find_elements(By.CLASS_NAME, "folder"):
+    if i.text != "":
+        lista_pasta_nivel_1.append(i.text)
+
+# print(lista_pasta_nivel_1)
+
+
 # TEM DE CLICAR NA PASTA PARA LOOPAR O CONTEUDO
 # Abrindo as pastas do sumário para acessar o conteúdo
 # Primerio nivel de pastas
@@ -90,32 +118,13 @@ def abrir_pastas(pastas: list):
 
 abrir_pastas(pastas=selecao_pasta_nivel_1)
 
-# Abrir o conteúdo de cada pasta
-# lista_nivel_adm_indireta = []
-# lista_licitacao = []
-# executivo = False
-# licitacao = False
-# for i in navegador.find_elements(By.CLASS_NAME, "folder"):
-#     if i.text == "EXECUTIVO":
-#         executivo = True
-#         continue
-#     if i.text == "LICITAÇÕES":
-#         licitacao = True
-#         continue
-#     if i.text != "" and licitacao is False:
-#         lista_nivel_adm_indireta.append(i.text)
-#     if i.text != "" and licitacao is True:
-#         lista_licitacao.append(i.text)
-
-# print(lista_nivel_adm_indireta)
-# print(lista_licitacao)
 
 lista_pasta_nivel_2 = []
 for i in navegador.find_elements(By.CLASS_NAME, "folder"):
     if i.text not in selecao_pasta_nivel_1 and i.text != "":
         lista_pasta_nivel_2.append(i.text)
 
-print(lista_pasta_nivel_2)
+# print(lista_pasta_nivel_2)
 
 # Construindo o dicionario da árvore de todas as pastas
 dict_pasta_nivel_2 = {}
@@ -126,7 +135,9 @@ for i in navegador.find_elements(By.CLASS_NAME, "folder"):
         if i.text in selecao_pasta_nivel_1:
             # Adicionando o primeiro nivel no dict
             count_nivel_1 = selecao_pasta_nivel_1.index(i.text)
-            if not dict_pasta_nivel_2:  # testando se o dict esta vazio, se true o dict esta vazio
+            if (
+                not dict_pasta_nivel_2
+            ):  # testando se o dict esta vazio, se true o dict esta vazio
                 dict_pasta_nivel_2.update({selecao_pasta_nivel_1[count_nivel_1]: {}})
                 continue
             elif bool(dict_pasta_nivel_2):  # se true o dict tem dados nele
@@ -134,19 +145,34 @@ for i in navegador.find_elements(By.CLASS_NAME, "folder"):
         #  NIVEL 2
         if i.text in lista_pasta_nivel_2:
             count_nivel_2 = lista_pasta_nivel_2.index(i.text)
-        if i.text == lista_pasta_nivel_2[count_nivel_2]:
+        if (
+            i.text == lista_pasta_nivel_2[count_nivel_2]
+            # Selecionando as pastas nivel 2 que terão dados coletadas
+            and i.text in selecao_pasta_nivel_2()
+        ):
             if not dict_pasta_nivel_2:  # se true o dict esta vazio
-                dict_pasta_nivel_2 = {selecao_pasta_nivel_1[count_nivel_1]: {i.text: {}}}
+                dict_pasta_nivel_2 = {
+                    selecao_pasta_nivel_1[count_nivel_1]: {i.text: {}}
+                }
             elif bool(dict_pasta_nivel_2):  # se true o dict tem dados nele
-                dict_pasta_nivel_2[selecao_pasta_nivel_1[count_nivel_1]].update({i.text: {}})
+                dict_pasta_nivel_2[selecao_pasta_nivel_1[count_nivel_1]].update(
+                    {i.text: {}}
+                )
 
-# TODO clicar no nivel 3 dos objetos da arvore
-executivo = False
-licitacao = False
-for i in navegador.find_elements(By.CLASS_NAME, "folder"):
-    if i.text not in selecao_pasta_nivel_1:
+# print(dict_pasta_nivel_2)
+
+# TODO clicar no nivel 2 das pastas selecionadas
+lista = navegador.find_elements(By.CLASS_NAME, "folder")
+for i in lista:
+    # Não clicar nas pastas que já estão abertas no nivel 1
+    nao_selecao_pasta_nivel_1 = set(lista_pasta_nivel_1) - set(selecao_pasta_nivel_1)
+    if (
+        i.text in lista_pasta_nivel_2
+        and i.text not in (nao_selecao_pasta_nivel_1)
+        # Selecionando as pastas nivel 2 que serão clicadas pelo usuário
+        and i.text in selecao_pasta_nivel_2()
+    ):
         if i.text != "":
-            print(i.text)
             try:
                 i.click()
             except Exception as e:
