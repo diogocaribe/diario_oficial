d415e9014c291ab4d914db6ce0eb3a0468e3e2a3
Diogo Caribé <dogosousa@gmail.com>
2024-07-23 18:26:09 -0300

    Adicionando a gravação da data que não existe

--- diario_oficial/diario_oficial.py
+++ diario_oficial/diario_oficial.py
@@ -23,17 +23,17 @@ from settings import Settings
 
 # Primeiro data do diário oficial 05/01/2016??
 
-site_link = 'https://dool.egba.ba.gov.br/'
-data_inicial = datetime.date(2024, 1, 18)  # 2024, 3, 15 tem um caso especial
+site_link = "https://dool.egba.ba.gov.br/"
+data_inicial = datetime.date(2024, 1, 15)  # 2024, 3, 15 tem um caso especial
 data_final = datetime.date(2024, 1, 16)
 
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
+chrome_options.add_argument("--headless")
 
 # LIsta de pastas que serão abertas para coleta de dados
 # Nível 1
-selecao_pasta_nivel_1 = ['EXECUTIVO']  # "LICITAÇÕES"
+selecao_pasta_nivel_1 = ["EXECUTIVO"]  # "LICITAÇÕES"
 
 # Nível 2 (Secretarias)
 # Não adicionar muitas secretarias por que isso causa um bug na renderização da árvore
@@ -42,7 +42,7 @@ selecao_pasta_nivel_2_executivo = [
     # "SECRETARIA DE RELAÇÕES INSTITUCIONAIS",
     # "SECRETARIA DA ADMINISTRAÇÃO",
     # 'PROCURADORIA GERAL DO ESTADO', # ver se consegue tirar a redundancia com LICITAÇÕES
-    'SECRETARIA DO MEIO AMBIENTE',
+    "SECRETARIA DO MEIO AMBIENTE",
     # "SECRETARIA DA EDUCAÇÃO",
     # "SECRETARIA DA FAZENDA",
 ]
@@ -77,16 +77,16 @@ def select_pasta_nivel_2(
 selecao_pasta_nivel_2 = select_pasta_nivel_2()
 
 # Separar atos das instituições
-tipo_ato = ['Portarias', 'Outros', 'Resoluções']
+tipo_ato = ["Portarias", "Outros", "Resoluções"]
 tipo_adm_direta = [
-    'Diretoria',
-    'Superintendência',
-    'Superintendências',
-    'Companhia',
-    'Departamento',
-    'Instituto',
-    'Policia',
-    'Corpo',
+    "Diretoria",
+    "Superintendência",
+    "Superintendências",
+    "Companhia",
+    "Departamento",
+    "Instituto",
+    "Policia",
+    "Corpo",
 ]
 
 
@@ -119,7 +119,7 @@ def listar_elmento(navegador, by: By, name: str):
     Returns:
         _type_: _description_
     """
-    return [i for i in navegador.find_elements(by, name) if i.text != '']
+    return [i for i in navegador.find_elements(by, name) if i.text != ""]
 
 
 def coletar_lista_link_ato(navegador, i):
@@ -130,15 +130,15 @@ def coletar_lista_link_ato(navegador, i):
     """
     time.sleep(0.5)
     i.click()
-    nt = namedtuple('ato', ['nome', 'identificador', 'link_conteudo'])
+    nt = namedtuple("ato", ["nome", "identificador", "link_conteudo"])
     lista_ato = [
         nt(
             i.text,
-            i.get_attribute('identificador'),
+            i.get_attribute("identificador"),
             f"https://dool.egba.ba.gov.br/apifront/portal/edicoes/publicacoes_ver_conteudo/{i.get_attribute('identificador')}",
         )
-        for i in listar_elmento(navegador, By.TAG_NAME, 'a')
-        if i.text[0] == '#'
+        for i in listar_elmento(navegador, By.TAG_NAME, "a")
+        if i.text[0] == "#"
     ]
     time.sleep(0.5)
     i.click()
@@ -155,7 +155,7 @@ def abrir_pastas(navegador, pastas: list):
         pastas (list): Opções de nome das pastas ['EXECUTIVO', 'LICITAÇÕES',
         'MUNICÍPIOS', 'DIVERSOS', 'ESPECIAL']
     """
-    for i in listar_elmento(navegador, By.CLASS_NAME, 'folder'):
+    for i in listar_elmento(navegador, By.CLASS_NAME, "folder"):
         if i.text in pastas:
             try:
                 time.sleep(0.5)
@@ -175,8 +175,8 @@ def coleta_diario_oficial(data: str) -> dict:
     Returns:
         dict: _description_
     """
-    data = data.strftime('%d-%m-%Y')
-    print(f'Diario oficial: {data}')
+    data_ = data.strftime("%d-%m-%Y")
+    print(f"Diario oficial: {data}")
     navegador = webdriver.Chrome(options=chrome_options)
 
     # Sintaxe do wait
@@ -191,50 +191,55 @@ def coleta_diario_oficial(data: str) -> dict:
     navegador.get(site_link)
 
     # Clicar no botão da versão html do diário oficial
-    navegador.find_element(By.ID, 'downloadHTML').click()
+    navegador.find_element(By.ID, "downloadHTML").click()
 
     # Janela para seleção de continuar na versão html
-    wdw.until(partial(esperar_elemento, By.CLASS_NAME, 'modal-footer'))
-    navegador.find_element(By.CLASS_NAME, 'modal-footer').find_element(
-        By.TAG_NAME, 'button'
+    wdw.until(partial(esperar_elemento, By.CLASS_NAME, "modal-footer"))
+    navegador.find_element(By.CLASS_NAME, "modal-footer").find_element(
+        By.TAG_NAME, "button"
     ).click()
 
     # Selecionando a data
-    print('Clear date input')
-    navegador.find_element(By.CLASS_NAME, 'date-input').clear()
+    print("Clear date input")
+    navegador.find_element(By.CLASS_NAME, "date-input").clear()
 
     time.sleep(0.5)
     navegador.switch_to.alert.accept()
-    print('Send date input')
-    navegador.find_element(By.CLASS_NAME, 'date-input').send_keys(data)
+    print("Send date input")
+    navegador.find_element(By.CLASS_NAME, "date-input").send_keys(data_)
 
-    print('Click date input')
-    navegador.find_element(By.CLASS_NAME, 'date-input').click()
+    print("Click date input")
+    navegador.find_element(By.CLASS_NAME, "date-input").click()
 
-    print('Click search button')
-    navegador.find_element(By.CLASS_NAME, 'fa-search').click()
+    print("Click search button")
+    navegador.find_element(By.CLASS_NAME, "fa-search").click()
 
     try:
         # print("Tratando a edição não existente")
         time.sleep(2)
         alert = wdw.until(lambda d: d.switch_to.alert)
-        if alert.text == 'Edição não existente!':
+        if alert.text == "Edição não existente!":
             alert.accept()
             time.sleep(1)
-            print(f'Edição não existe: {data}')
-            return
+            print(f"Edição não existe: {data_}")
+            return {
+                'data': data,
+                'exist': False
+                }
     except Exception:
-        print(f'Edição existente: {data}')
+        print(f"Edição existente: {data_}")
 
     # Janela para seleção de continuar na versão html
-    wdw.until(partial(esperar_elemento, By.CLASS_NAME, 'modal-footer'))
-    navegador.find_element(By.CLASS_NAME, 'modal-footer').find_element(
-        By.TAG_NAME, 'button'
+    wdw.until(partial(esperar_elemento, By.CLASS_NAME, "modal-footer"))
+    navegador.find_element(By.CLASS_NAME, "modal-footer").find_element(
+        By.TAG_NAME, "button"
     ).click()
 
     ######################## NIVEL 1 ########################
     # Listar pastas no nivel 1 do sumário
-    lista_pasta_nivel_1 = [i.text for i in listar_elmento(navegador, By.CLASS_NAME, 'folder')]
+    lista_pasta_nivel_1 = [
+        i.text for i in listar_elmento(navegador, By.CLASS_NAME, "folder")
+    ]
     nao_selecao_pasta_nivel_1 = set(lista_pasta_nivel_1) - set(selecao_pasta_nivel_1)
 
     abrir_pastas(navegador, pastas=selecao_pasta_nivel_1)
@@ -245,19 +250,21 @@ def coleta_diario_oficial(data: str) -> dict:
     #########################################################
     lista_pasta_nivel_2 = [
         i.text
-        for i in listar_elmento(navegador, By.CLASS_NAME, 'folder')
+        for i in listar_elmento(navegador, By.CLASS_NAME, "folder")
         if i.text not in lista_pasta_nivel_1
     ]
 
     # Construindo o dicionario da árvore de todas as pastas nivel 2
     dict_pasta_nivel_2 = {}
     count_nivel_2 = 0
-    for i in listar_elmento(navegador, By.CLASS_NAME, 'folder'):
+    for i in listar_elmento(navegador, By.CLASS_NAME, "folder"):
         #  NIVEL 1
         if i.text in selecao_pasta_nivel_1:
             # Adicionando o primeiro nivel no dict
             count_nivel_1 = selecao_pasta_nivel_1.index(i.text)
-            if not dict_pasta_nivel_2:  # testando se o dict esta vazio, se true o dict esta vazio
+            if (
+                not dict_pasta_nivel_2
+            ):  # testando se o dict esta vazio, se true o dict esta vazio
                 dict_pasta_nivel_2.update({selecao_pasta_nivel_1[count_nivel_1]: {}})
                 pass
             if bool(dict_pasta_nivel_2):  # se true o dict tem dados nele
@@ -271,9 +278,13 @@ def coleta_diario_oficial(data: str) -> dict:
             and i.text in select_pasta_nivel_2()
         ):
             if not dict_pasta_nivel_2:  # se true o dict esta vazio
-                dict_pasta_nivel_2 = {selecao_pasta_nivel_1[count_nivel_1]: {i.text: {}}}
+                dict_pasta_nivel_2 = {
+                    selecao_pasta_nivel_1[count_nivel_1]: {i.text: {}}
+                }
             elif bool(dict_pasta_nivel_2):  # se true o dict tem dados nele
-                dict_pasta_nivel_2[selecao_pasta_nivel_1[count_nivel_1]].update({i.text: {}})
+                dict_pasta_nivel_2[selecao_pasta_nivel_1[count_nivel_1]].update(
+                    {i.text: {}}
+                )
 
     print(dict_pasta_nivel_2)
 
@@ -285,12 +296,11 @@ def coleta_diario_oficial(data: str) -> dict:
     ############# AUTARQUIAS, SUPERINTENDENCIA ##############
     #########################################################
     # Lista nivel 3 das pastas
-    print('Construindo pastas nivel 3')
+    print("Construindo pastas nivel 3")
     lista_pasta_nivel_3 = [
         i.text
-        for i in listar_elmento(navegador, By.CLASS_NAME, 'folder')
-        if i.text not in lista_pasta_nivel_1
-        and i.text not in lista_pasta_nivel_2
+        for i in listar_elmento(navegador, By.CLASS_NAME, "folder")
+        if i.text not in lista_pasta_nivel_1 and i.text not in lista_pasta_nivel_2
         # Todas as pasta nivel 3 tem padrão camel case (ex. Portaria)
         and i.text[1].islower()
     ]
@@ -300,7 +310,7 @@ def coleta_diario_oficial(data: str) -> dict:
     count_nivel_1 = 0
     count_nivel_2 = 0
     count_nivel_3 = 0
-    for i in listar_elmento(navegador, By.CLASS_NAME, 'folder'):
+    for i in listar_elmento(navegador, By.CLASS_NAME, "folder"):
         #  NIVEL 1
         if i.text in selecao_pasta_nivel_1:
             # Adicionando o primeiro nivel no dict
@@ -354,14 +364,16 @@ def coleta_diario_oficial(data: str) -> dict:
                     pass
 
     # Clicando nas no nivel 3 (Autarquias, Superintendencias, Diretorias)
-    lista_pasta_clicar = set(lista_pasta_nivel_3) - set(lista_pasta_nivel_3).intersection(tipo_ato)
+    lista_pasta_clicar = set(lista_pasta_nivel_3) - set(
+        lista_pasta_nivel_3
+    ).intersection(tipo_ato)
     abrir_pastas(navegador, set(lista_pasta_clicar))
     #########################################################
     ######################## NIVEL 4 ########################
     ######################### ATOS ##########################
     #########################################################
-    print('Construindo pastas nivel 4')
-    lista_elemento_pasta = listar_elmento(navegador, By.CLASS_NAME, 'folder')
+    print("Construindo pastas nivel 4")
+    lista_elemento_pasta = listar_elmento(navegador, By.CLASS_NAME, "folder")
     lista_nivel_4_ = {
         i.text
         for i in lista_elemento_pasta
@@ -381,7 +393,7 @@ def coleta_diario_oficial(data: str) -> dict:
     count_nivel_2 = 0
     count_nivel_3 = 0
     count_nivel_4 = 0
-    for i in listar_elmento(navegador, By.CLASS_NAME, 'folder'):
+    for i in listar_elmento(navegador, By.CLASS_NAME, "folder"):
         #  NIVEL 1
         if i.text in selecao_pasta_nivel_1:
             # Adicionando o primeiro nivel no dict
@@ -426,7 +438,13 @@ def coleta_diario_oficial(data: str) -> dict:
             count_nivel_4 = 0
 
     print(dict_pasta_nivel_4)
-    return dict_pasta_nivel_4
+    return {
+        "diario_oficial_json": dict_pasta_nivel_4,
+        "edicao": 12345,
+        "data": data,
+        "exist": True,
+    }
+    
 
 
 def coletar_dado_data(data_inicial, data_final):
@@ -435,21 +453,10 @@ def coletar_dado_data(data_inicial, data_final):
         data_inicial += datetime.timedelta(days=1)
 
 
-def json_carga_banco():
-    """_summary_"""
-    json_ = {
-        'diario_oficial_json': coleta_diario_oficial(data_inicial),
-        'edicao': 12345,
-        'data': data_inicial,
-        'exist': True,
-    }
-    return json_
-
-
-def carga_banco():
+def carga_banco(data: str):
     engine = create_engine(Settings().DATABASE_URL, echo=True)
 
-    a = json_carga_banco()
+    a = coleta_diario_oficial(data=data)
 
     with Session(engine) as session:
         dados = DiarioOficialBruto(**a)
@@ -458,4 +465,4 @@ def carga_banco():
         session.commit()
 
 
-carga_banco()
+carga_banco(data=data_inicial)
