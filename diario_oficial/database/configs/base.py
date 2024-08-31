9279f8a6560aeffb96d1543d9b93f9d657492d06
Diogo Caribé <dogosousa@gmail.com>
2024-08-31 09:18:58 -0300

    Adicionando o padrão de nomenclatura

--- diario_oficial/database/configs/base.py
+++ diario_oficial/database/configs/base.py
@@ -1,4 +1,12 @@
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import MetaData
 
+metadata = MetaData(naming_convention={
+    "ix": "ix_%(table_name)s_%(column_0_name)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s_%(column_0_name)s"
+})
 
-Base = declarative_base()
+Base = declarative_base(metadata=metadata)
