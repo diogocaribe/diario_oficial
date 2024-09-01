74cbf9785fb3a63728185e0e0b8badc14a3e27cd
Diogo Caribé <dogosousa@gmail.com>
2024-09-01 11:36:55 -0300

    Banco de dados montados para explodir os dados brutos

--- diario_oficial/database/entity/dominio.py
+++ diario_oficial/database/entity/dominio.py
@@ -15,3 +15,55 @@ class Poder(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
     nome: Mapped[str] = mapped_column(nullable=False, unique=True)
+
+
+class AdministracaoDireta(Base):
+    """Tabela para registro do dominio da adm direta (Secretarias)
+        Ex.
+        nome : Secretaria de Meio Ambiente
+        sigla: SEMA
+
+    Args:
+        Base (_type_): _description_
+    """
+
+    __tablename__ = 'adm_direta'
+    __table_args__ = {'schema': 'dominio'}
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    nome: Mapped[str] = mapped_column(nullable=False, unique=True)
+    sigla: Mapped[str] = mapped_column(nullable=False)
+
+
+class AdministracaoIndireta(Base):
+    """Tabela para registro do dominio da adm indireta (Orgãos, Institutos)
+        Ex.
+        nome : Instituto de Meio Ambiente e Recursos Hídricos
+        sigla: INEMA
+    Args:
+        Base (_type_): _description_
+    """
+
+    __tablename__ = 'adm_indireta'
+    __table_args__ = {'schema': 'dominio'}
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    nome: Mapped[str] = mapped_column(nullable=False, unique=True)
+    sigla: Mapped[str] = mapped_column(nullable=False)
+
+
+class OrgaoAdministracaoDireta(Base):
+    """Tabela para registro do dominio da adm indireta (Orgãos, Institutos)
+        Ex.
+        nome : Diretoria
+        sigla : DG
+    Args:
+        Base (_type_): _description_
+    """
+
+    __tablename__ = 'orgao_adm_indireta'
+    __table_args__ = {'schema': 'dominio'}
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True, index=True)
+    nome: Mapped[str] = mapped_column(nullable=False, unique=True)
+    sigla: Mapped[str] = mapped_column(nullable=False)
