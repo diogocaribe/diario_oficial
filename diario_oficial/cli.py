d629821c645549fad4b092824c8d9ebf731c1279
Diogo Caribé <dogosousa@gmail.com>
2024-10-30 08:07:16 -0300

    feature: adicionando cli

--- diario_oficial/cli.py
+++ diario_oficial/cli.py
@@ -1,8 +1,31 @@
-from typer import run
+import typer
+from datetime import datetime
+from rich.pretty import Pretty
+from rich import print
+from raspar_doe import raspar_diario_oficial
+
+from contextlib import redirect_stdout
+from io import StringIO
+
+app = typer.Typer()
 
 
-def raspar_doe():
-    print('Rodando raspar doe')
+@app.command()
+def hello(name: str):
+    print(f'Hello {name}')
 
 
-run(raspar_doe)
+@app.command()
+def raspardoe(data: str):
+    print(f'Raspando os dados do Diário Oficial Bahia de {data}')
+    # Redireciona temporariamente a saída para uma variável
+    with redirect_stdout(StringIO()):
+        data = datetime.strptime(data, '%d-%m-%Y')
+        resultado = raspar_diario_oficial(data)  # Os prints serão suprimidos
+
+    # Exibe apenas o retorno final, sem os prints internos da função
+    print(Pretty(resultado))
+
+
+if __name__ == '__main__':
+    app()
