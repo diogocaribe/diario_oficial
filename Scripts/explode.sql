bd2c3a8beb3265cd8e8f5b89f670128083174725
Diogo Caribé <dogosousa@gmail.com>
2024-11-28 16:41:25 -0300

    refactor: modificando a consulta para incorporar o nivel 5

--- Scripts/explode.sql
+++ Scripts/explode.sql
@@ -1,79 +1,118 @@
--- Consulta para fazer a carga em processing.publicacao
 WITH poder AS (
-    -- Extrai as chaves do primeiro nível (por exemplo, "EXECUTIVO")
-    SELECT
-        id AS doe_bruto_id,
-        jsonb_object_keys(doe_json) AS poder,
-        doe_json -> jsonb_object_keys(doe_json) AS _json
-    FROM processing.doe_bruto
-    WHERE dt_edicao = '2024-01-06'
+  -- Extrai as chaves do primeiro nível (por exemplo, "EXECUTIVO")
+  SELECT
+      id AS doe_bruto_id,
+      jsonb_object_keys(doe_json) AS poder,
+      doe_json -> jsonb_object_keys(doe_json) AS _json
+  FROM processing.doe_bruto
+  WHERE dt_edicao = '2024-02-09'
 ),
 adm_direta AS (
-    -- Extrai as chaves do segundo nível a partir do resultado da CTE anterior
-    SELECT
-    	doe_bruto_id,
-        poder,
-        jsonb_object_keys(_json) AS adm_direta,
-        _json -> jsonb_object_keys(_json) AS _json
-    FROM poder
+     -- Extrai as chaves do segundo nível a partir do resultado da CTE anterior
+     SELECT
+         doe_bruto_id,
+         poder,
+         jsonb_object_keys(_json) AS adm_direta,
+         _json -> jsonb_object_keys(_json) AS _json
+     FROM poder
 ),
 divisao_adm_direta_ AS (
-    SELECT
-		doe_bruto_id,
-        poder,
-        adm_direta,
-        jsonb_object_keys(_json) AS divisao_adm_direta,
-        _json -> jsonb_object_keys(_json) AS _json
-    FROM adm_direta
+     -- Extrai as chaves do segundo nível a partir do resultado da CTE anterior
+     SELECT
+         doe_bruto_id,
+         poder,
+         adm_direta,
+         jsonb_object_keys(_json) AS divisao_adm_direta,
+         _json -> jsonb_object_keys(_json) AS _json
+     FROM adm_direta
 ),
 tratando_divisao_adm_direta AS (
+     SELECT
+         doe_bruto_id,
+         poder,
+         adm_direta,
+         CASE
+             WHEN divisao_adm_direta IN (SELECT dad.nome FROM dominio.divisao_adm_direta dad) THEN divisao_adm_direta
+         END AS divisao_adm_direta,
+         CASE
+             WHEN divisao_adm_direta IN (SELECT ai.nome FROM dominio.adm_indireta ai) THEN divisao_adm_direta
+         END AS adm_indireta,
+         CASE
+             WHEN divisao_adm_direta IN (SELECT tp.nome FROM dominio.tipo_publicacao tp) THEN divisao_adm_direta
+         END AS tipo_publicacao,
+         CASE 
+			WHEN 
+				jsonb_typeof(_json) = 'object' THEN _json
+		 END AS _json,
+		 CASE 
+			WHEN 
+				jsonb_typeof(_json) = 'array' THEN _json
+		 END AS conteudo
+     FROM divisao_adm_direta_
+),
+tratanto_divisao_adm_indireta AS (
+	SELECT 
+		doe_bruto_id,
+	    poder,
+        adm_direta,
+        divisao_adm_direta,
+        adm_indireta,
+		CASE 
+			WHEN divisao_adm_indireta IN (SELECT dai.nome FROM dominio.divisao_adm_indireta dai) THEN divisao_adm_indireta
+		END AS divisao_adm_indireta,
+        _json
+	FROM (	
+		SELECT
+			doe_bruto_id,
+		    poder,
+	        adm_direta,
+	        divisao_adm_direta,
+	        adm_indireta,
+	        jsonb_object_keys(_json) AS divisao_adm_indireta,
+	        _json -> jsonb_object_keys(_json) AS _json     
+		FROM tratando_divisao_adm_direta
+	) t
+	WHERE divisao_adm_indireta IN (SELECT nome FROM dominio.divisao_adm_indireta dai)
+),
+tratanto_divisao_adm_indireta_ AS (
 	SELECT
 		doe_bruto_id,
-        poder,
+	    poder,
         adm_direta,
-        CASE 
-        	WHEN divisao_adm_direta IN (SELECT dad.nome FROM dominio.divisao_adm_direta dad) THEN divisao_adm_direta
-        END AS divisao_adm_direta,
-        CASE 
-        	WHEN divisao_adm_direta IN (SELECT ai.nome FROM dominio.adm_indireta ai) THEN divisao_adm_direta
-        END AS adm_indireta,
-        CASE 
-        	WHEN divisao_adm_direta IN (SELECT tp.nome FROM dominio.tipo_publicacao tp) THEN divisao_adm_direta
-        END AS tipo_publicacao,
-        CASE 
-        	WHEN
-	        	jsonb_typeof(_json) = 'array' THEN _json
-	        ELSE
-	        	_json
-        END AS _json
-    FROM divisao_adm_direta_
+        divisao_adm_direta,
+        adm_indireta,
+        divisao_adm_indireta,
+		jsonb_object_keys(_json) AS tipo_publicacao,
+		_json -> jsonb_object_keys(_json) AS _json 
+	FROM tratanto_divisao_adm_indireta
 )
 --Administracao Direta
 SELECT
 	doe_bruto_id,
 	p.id AS poder_id,
-    ad.id AS adm_direta_id,
+	ad.id AS adm_direta_id,
 	dad.id AS divisao_adm_direta_id,
 	ai.id AS adm_indireta_id,
+	NULL AS divisao_adm_indireta,
 	tp.id AS tipo_publicacao_id,
-	jsonb_array_elements_text(_json)::jsonb->>'nome' AS nome_ato,
-	jsonb_array_elements_text(_json)::jsonb->>'identificador' AS identificador_link,
-	jsonb_array_elements_text(_json)::jsonb->>'link' AS link 
+	jsonb_array_elements_text(conteudo)::jsonb->>'nome' AS nome_ato,
+	jsonb_array_elements_text(conteudo)::jsonb->>'identificador' AS identificador_link,
+	jsonb_array_elements_text(conteudo)::jsonb->>'link' AS link
 FROM tratando_divisao_adm_direta
 LEFT JOIN dominio.poder p ON p.nome = poder
 LEFT JOIN dominio.adm_direta ad ON ad.nome = adm_direta
 LEFT JOIN dominio.divisao_adm_direta dad ON dad.nome = divisao_adm_direta
 LEFT JOIN dominio.adm_indireta ai ON ai.nome = adm_indireta
 LEFT JOIN dominio.tipo_publicacao tp ON tp.nome = tipo_publicacao
-WHERE jsonb_typeof(_json) = 'array'
 UNION
--- Administracao Indireta
+-- Administracao Indireta com 4 niveis
 SELECT 
 	doe_bruto_id,
 	poder_id,
     adm_direta_id,
     divisao_adm_direta_id,
     adm_indireta_id,
+    divisao_adm_indireta,
 	tp2.id AS tipo_publicacao_id,
 	t.nome AS nome_ato, 
 	identificador AS identificador_link, 
@@ -85,6 +124,7 @@ FROM (
 	    ad.id AS adm_direta_id,
 	    dad.id AS divisao_adm_direta_id,
 	    ai.id AS adm_indireta_id,
+	    NULL::NUMERIC AS divisao_adm_indireta,
 	    jsonb_object_keys(_json) AS tipo_publicacao,
 	    jsonb_array_elements_text(_json -> jsonb_object_keys(_json))::jsonb->>'nome' AS nome,
 	    jsonb_array_elements_text(_json -> jsonb_object_keys(_json))::jsonb->>'identificador' AS identificador,
@@ -97,7 +137,27 @@ FROM (
 	LEFT JOIN dominio.tipo_publicacao tp ON tp.nome = tipo_publicacao
 	WHERE jsonb_typeof(_json) = 'object'
 ) t
-LEFT JOIN dominio.tipo_publicacao tp2 ON tp2.nome = t.tipo_publicacao;
+LEFT JOIN dominio.tipo_publicacao tp2 ON tp2.nome = t.tipo_publicacao
+-- Administracao Indireta com 5 niveis
+UNION
+SELECT
+	doe_bruto_id,
+    p.id  AS poder_id,
+    ad.id AS adm_direta_id,
+    dad.id AS divisao_adm_direta_id,
+    ai.id AS adm_indireta_id,
+    dai.id AS divisao_adm_indireta,
+	tp.id AS tipo_publicacao_id,
+	jsonb_array_elements_text(_json)::jsonb->>'nome' AS nome_ato,
+	jsonb_array_elements_text(_json)::jsonb->>'identificador' AS identificador_link,
+	jsonb_array_elements_text(_json)::jsonb->>'link' AS link
+FROM tratanto_divisao_adm_indireta_
+LEFT JOIN dominio.poder p ON p.nome = poder
+LEFT JOIN dominio.adm_direta ad ON ad.nome = adm_direta
+LEFT JOIN dominio.divisao_adm_direta dad ON dad.nome = divisao_adm_direta
+LEFT JOIN dominio.adm_indireta ai ON ai.nome = adm_indireta
+LEFT JOIN dominio.divisao_adm_indireta dai ON dai.nome = divisao_adm_indireta
+LEFT JOIN dominio.tipo_publicacao tp ON tp.nome = tipo_publicacao;
 
 
 -- Consulta para fazer o join dos atributos de publicacao
